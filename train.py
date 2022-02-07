ff7004894d9caa4a7aec3a3dfc4a37a1dc13a337
ShathaMelhem <48318208+ShathaMelhem@users.noreply.github.com>
2022-02-07 04:38:24 +0200

    Create train.py

---
+++ train.py
@@ -0,0 +1,120 @@
+import tensorflow as tf
+from model.build import build_model
+"""
+This code is to build and train 2D U-Net
+"""
+import numpy as np
+import sys
+import subprocess
+import argparse
+import os
+
+from keras.models import Model
+from keras.layers import Input, Activation, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, ZeroPadding2D, add
+from tensorflow.keras.optimizers import Adam, SGD
+from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras import backend as K
+from keras import losses
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+
+from utils import *
+from data import load_train_data
+
+K.set_image_data_format('channels_last')  # Tensorflow dimension ordering
+
+# ----- paths setting -----
+data_path = sys.argv[1] + "/"
+model_path = data_path + "models/"
+log_path = data_path + "logs/"
+
+
+# ----- params for training and testing -----
+batch_size = 1
+cur_fold = sys.argv[2]
+plane = sys.argv[3]
+epoch = int(sys.argv[4])
+init_lr = float(sys.argv[5])
+
+
+# ----- Dice Coefficient and cost function for training -----
+smooth = 1.
+
+ef dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2.0 * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    return  -dice_coef(y_true, y_pred)
+
+def train(fold, plane, batch_size, nb_epoch,init_lr):
+    """
+    train an Unet model with data from load_train_data()
+    Parameters
+    ----------
+    fold : string
+        which fold is experimenting in 4-fold. It should be one of 0/1/2/3
+    plane : char
+        which plane is experimenting. It is from 'X'/'Y'/'Z'
+    batch_size : int
+        size of mini-batch
+    nb_epoch : int
+        number of epochs to train NN
+    init_lr : float
+        initial learning rate
+    """
+
+    print ("number of epoch: ", nb_epoch)
+    print ("learning rate: ", init_lr)
+
+    # --------------------- load and preprocess training data -----------------
+    print ('-'*80)
+    print ('         Loading and preprocessing train data...')
+    print ('-'*80)
+
+    imgs_train, imgs_mask_train = load_train_data(fold, plane)
+
+    imgs_row = imgs_train.shape[1]
+    imgs_col = imgs_train.shape[2]
+
+    imgs_train = preprocess(imgs_train)
+    imgs_mask_train = preprocess(imgs_mask_train)
+
+    imgs_train = imgs_train.astype('float32')
+    imgs_mask_train = imgs_mask_train.astype('float32')
+
+    # ---------------------- Create, compile, and train model ------------------------
+    print ('-'*80)
+    print ('		Creating and compiling model...')
+    print ('-'*80)
+
+    model = build_model('convnext_tiny', num_classes=2)
+    model.compile(optimizer=Adam(lr=init_lr), loss=dice_coef_loss, metrics=[dice_coef])
+    print (model.summary())
+
+    print ('-'*80)
+    print ('		Fitting model...')
+    print ('-'*80)
+
+    ver = 'convnext_fd%s_%s_ep%s_lr%s.csv'%(cur_fold, plane, epoch, init_lr)
+    csv_logger = CSVLogger(log_path + ver)
+    model_checkpoint = ModelCheckpoint(model_path + ver + ".h5",
+                                       monitor='loss',
+                                       save_best_only=False,
+                                       period=10)
+
+    history = model.fit(imgs_train, imgs_mask_train,
+                        batch_size= batch_size, epochs= nb_epoch, verbose=1, shuffle=True,
+                        callbacks=[model_checkpoint, csv_logger])
+
+
+if __name__ == "__main__":
+
+    train(cur_fold, plane, batch_size, epoch, init_lr)
+
+    print ("training done")
