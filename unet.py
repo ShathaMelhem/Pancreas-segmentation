145021f71ae6d4cccd9a37a78b7bc6768bb5d725
Shatha Melhem <48318208+ShathaMelhem@users.noreply.github.com>
2022-04-29 21:55:08 +0300

    Update unet.py

--- unet.py
+++ unet.py
@@ -61,36 +61,36 @@ def get_unet(img_rows, img_cols, flt=64, pool_size=(2, 2, 2), init_lr=1.0e-5):
     conv1 = Conv2D(flt, (3, 3), activation='relu', padding='same')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Conv2D(flt*2, (3, 3), activation='gelu', padding='same')(pool1)
-    conv2 = Conv2D(flt*2, (3, 3), activation='gelu', padding='same')(conv2)
+    conv2 = Conv2D(flt*2, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(flt*2, (3, 3), activation='relu', padding='same')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
     conv3 = Conv2D(flt*4, (3, 3), activation='gelu', padding='same')(pool2)
     conv3 = Conv2D(flt*4, (3, 3), activation='gelu', padding='same')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Conv2D(flt*8, (3, 3), activation='gelu', padding='same')(pool3)
-    conv4 = Conv2D(flt*8, (3, 3), activation='gelu', padding='same')(conv4)
+    conv4 = Conv2D(flt*8, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(flt*8, (3, 3), activation='relu', padding='same')(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-    conv5 = Conv2D(flt*16, (3, 3), activation='gelu', padding='same')(pool4)
-    conv5 = Conv2D(flt*8, (3, 3), activation='gelu', padding='same')(conv5)
+    conv5 = Conv2D(flt*16, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(flt*8, (3, 3), activation='relu', padding='same')(conv5)
 
     up6 = concatenate([Conv2DTranspose(flt*8, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
-    conv6 = Conv2D(flt*8, (3, 3), activation='gelu', padding='same')(up6)
-    conv6 = Conv2D(flt*4, (3, 3), activation='gelu', padding='same')(conv6)
+    conv6 = Conv2D(flt*8, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(flt*4, (3, 3), activation='relu', padding='same')(conv6)
 
     up7 = concatenate([Conv2DTranspose(flt*4, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
-    conv7 = Conv2D(flt*4, (3, 3), activation='gelu', padding='same')(up7)
-    conv7 = Conv2D(flt*2, (3, 3), activation='gelu', padding='same')(conv7)
+    conv7 = Conv2D(flt*4, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(flt*2, (3, 3), activation='relu', padding='same')(conv7)
 
     up8 = concatenate([Conv2DTranspose(flt*2, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
-    conv8 = Conv2D(flt*2, (3, 3), activation='gelu', padding='same')(up8)
-    conv8 = Conv2D(flt, (3, 3), activation='gelu', padding='same')(conv8)
+    conv8 = Conv2D(flt*2, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(flt, (3, 3), activation='relu', padding='same')(conv8)
 
     up9 = concatenate([Conv2DTranspose(flt, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
-    conv9 = Conv2D(flt, (3, 3), activation='gelu', padding='same')(up9)
-    conv9 = Conv2D(flt, (3, 3), activation='gelu', padding='same')(conv9)
+    conv9 = Conv2D(flt, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(flt, (3, 3), activation='relu', padding='same')(conv9)
 
     conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
 
